f9c588711e0a4c669ef2001e43c7cd5ef93948e0
V1CT0RESP <102185829+V1CT0RESP@users.noreply.github.com>
2022-03-23 20:13:17 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Menu Default Modified by V1CT0R#4220'
+
+version '1.0.4'
+
+client_scripts {
+	'@es_extended/client/wrapper.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+	'html/fonts/*.ttf',
+	'html/img/*.png'
+}
+
+dependencies {
+	'es_extended'
+}
