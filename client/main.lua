16cbfbf84dbeed9b254f9aa12d0b5014c415b16b
V1CT0R <102185829+V1CT0RESP@users.noreply.github.com>
2022-03-26 15:33:23 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,6 @@
-ESX = nil
+local ESX = exports["es_extended"]:getSharedObject()
 
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-
 	local Keys = {
 		["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
 		["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
